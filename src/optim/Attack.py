1673caebfdd98d7e9b90c0d53d8a97a2a9f46b95
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-02-05 21:24:03 +0330

    t

--- src/optim/Attack.py
+++ src/optim/Attack.py
@@ -39,3 +39,29 @@ def pgd(model, inputs, c, epsilon, alpha, num_iter):
         delta.grad.zero_()
     
     return delta.detach()
+
+
+upper_limit, lower_limit = 1,0
+
+def clamp(X, lower_limit, upper_limit):
+    return torch.max(torch.min(X, upper_limit), lower_limit)
+
+def pgd_inf( model, inputs, c, epsilon, alpha, num_iter,norm='l_inf'):
+    delta = torch.zeros_like(inputs).to(device)
+    if norm == "l_inf":
+            delta.uniform_(-epsilon, epsilon)
+    delta = clamp(delta, lower_limit-inputs, upper_limit-inputs)
+    delta.requires_grad = True
+    for _ in range(num_iter):
+        outputs = model(inputs+delta)
+        dist = torch.sum((outputs - c) ** 2, dim=1)
+        scores=dist 
+    
+        scores.backward()        
+        
+        if norm == "l_inf":
+            delta.data = torch.clamp(delta + alpha * torch.sign(delta.grad.data), min=-epsilon, max=epsilon)
+        delta.data = clamp(delta, lower_limit - inputs, upper_limit - inputs)
+        delta.grad.zero_()
+    
+    return delta.detach()
