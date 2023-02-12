71aea797450fa5de10a11b35f06599a5de9461fc
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-02-12 20:36:48 +0330

    test

--- src/networks/mvtec_LeNet.py
+++ src/networks/mvtec_LeNet.py
@@ -7,72 +7,69 @@ from base.base_net import BaseNet
 
 class MVTec_LeNet(BaseNet):
 
-    def __init__(self, rep_dim=64):
+    def __init__(self, rep_dim=128):
         super().__init__()
 
         self.rep_dim = rep_dim
-        self.encoder=nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=5),
-            nn.ReLU(True),
-            nn.Conv2d(64, 192, kernel_size=5, stride=3),
-            nn.ReLU(True),
-            nn.Conv2d(192, 384, kernel_size=5, stride=1),
-            nn.ReLU(True),
-            nn.Conv2d(384, 256, kernel_size=3, stride=3),
-            nn.ReLU(True),
-            nn.Conv2d(256, 50, kernel_size=4, stride=1),
-            nn.ReLU(True))
+        self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(50, self.rep_dim, bias=False)
+        self.conv1 = nn.Conv2d(3, 32, 5, bias=False, padding=2)
+        self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
+        self.conv2 = nn.Conv2d(32, 64, 5, bias=False, padding=2)
+        self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
+        self.conv3 = nn.Conv2d(64, 128, 5, bias=False, padding=2)
+        self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
+        self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=False)
+
     def forward(self, x):
-        x = x.view(-1, 3, 256, 256)
-        
-        x=self.encoder(x)
-        
-        x = x.view(x.size(0), -1)
-        
-        x=self.fc1(x)
-        x=nn.ReLU(True)(x)   
-        
+        x = x.view(-1, 3, 32, 32)
+        x = self.conv1(x)
+        x = self.pool(F.leaky_relu(self.bn2d1(x)))
+        x = self.conv2(x)
+        x = self.pool(F.leaky_relu(self.bn2d2(x)))
+        x = self.conv3(x)
+        x = self.pool(F.leaky_relu(self.bn2d3(x)))
+        x = x.view(int(x.size(0)), -1)
+        x = self.fc1(x)
         return x
 
 
 class MVTec_LeNet_Decoder(BaseNet):
 
-    def __init__(self, rep_dim=64):
+    def __init__(self, rep_dim=128):
         super().__init__()
 
         self.rep_dim = rep_dim
 
-        self.decoder=nn.Sequential(
-            
-            nn.ConvTranspose2d(50, 256, kernel_size=4, stride=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(256, 384, kernel_size=3, stride=3),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(384, 192, kernel_size=5, stride=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(192, 64, kernel_size=5, stride=3),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(64, 3, kernel_size=11, stride=5),
-            nn.ReLU(True))
-    
-        self.fc2 = nn.Linear(self.rep_dim, 50, bias=False)
+        self.deconv1 = nn.ConvTranspose2d(int(self.rep_dim / (4 * 4)), 128, 5, bias=False, padding=2)
+        nn.init.xavier_uniform_(self.deconv1.weight, gain=nn.init.calculate_gain('leaky_relu'))
+        self.bn2d4 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, 5, bias=False, padding=2)
+        nn.init.xavier_uniform_(self.deconv2.weight, gain=nn.init.calculate_gain('leaky_relu'))
+        self.bn2d5 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
+        self.deconv3 = nn.ConvTranspose2d(64, 32, 5, bias=False, padding=2)
+        nn.init.xavier_uniform_(self.deconv3.weight, gain=nn.init.calculate_gain('leaky_relu'))
+        self.bn2d6 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
+        self.deconv4 = nn.ConvTranspose2d(32, 3, 5, bias=False, padding=2)
+        nn.init.xavier_uniform_(self.deconv4.weight, gain=nn.init.calculate_gain('leaky_relu'))
 
     def forward(self, x):
-        
-        x=self.fc2(x)
-        x=nn.ReLU(True)(x)
-        x = x.view(x.size(0), 50, 1, 1)
-        
-        x=self.decoder(x)
-        
+        x = x.view(int(x.size(0)), int(self.rep_dim / (4 * 4)), 4, 4)
+        x = F.leaky_relu(x)
+        x = self.deconv1(x)
+        x = F.interpolate(F.leaky_relu(self.bn2d4(x)), scale_factor=2)
+        x = self.deconv2(x)
+        x = F.interpolate(F.leaky_relu(self.bn2d5(x)), scale_factor=2)
+        x = self.deconv3(x)
+        x = F.interpolate(F.leaky_relu(self.bn2d6(x)), scale_factor=2)
+        x = self.deconv4(x)
+        x = torch.sigmoid(x)
         return x
 
 
 class MVTec_LeNet_Autoencoder(BaseNet):
 
-    def __init__(self, rep_dim=64):
+    def __init__(self, rep_dim=128):
         super().__init__()
 
         self.rep_dim = rep_dim
