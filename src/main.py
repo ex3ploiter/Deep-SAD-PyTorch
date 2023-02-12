9b9295ee0edaab7b7b20cfad4fa2a515f5eac40f
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-02-12 20:09:37 +0330

    bug

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ import os
 @click.command()
 @click.argument('dataset_name', type=click.Choice(['mnist', 'fmnist', 'cifar10','svhn','mvtec', 'arrhythmia', 'cardio', 'satellite',
                                                    'satimage-2', 'shuttle', 'thyroid']))
-@click.argument('net_name', type=click.Choice(['mnist_LeNet', 'fmnist_LeNet', 'cifar10_LeNet', 'arrhythmia_mlp',
+@click.argument('net_name', type=click.Choice(['mnist_LeNet', 'fmnist_LeNet', 'cifar10_LeNet', 'arrhythmia_mlp','mvtec_LeNet',
                                                'cardio_mlp', 'satellite_mlp', 'satimage-2_mlp', 'shuttle_mlp',
                                                'thyroid_mlp']))
 @click.argument('xp_path', type=click.Path(exists=True))
